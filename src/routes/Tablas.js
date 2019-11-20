f17b645fd19b42ff249807f0c45284fa534297dc
98kevin <kevinfuentes201730303@cunoc.edu.gt>
2019-11-20 06:56:21 -0600

    correccion de errores en la logia

--- src/routes/Tablas.js
+++ src/routes/Tablas.js
@@ -36,3 +36,236 @@ function crearTablas(tablaIdentificadores, tablaTokens, tablaLiterales, tablaCon
     
         return retorno;
     }
+
+
+
+
+/**
+ * Função inserirToken: insere um novo token na tabla unica.
+ * @params:	tablaTokens (tabla unica)
+ * 			idToken 	(codigo definido para o token)
+ * 			linea 		(linea do token no codigo)
+ * 			coluna		(coluna do token no codigo)
+ * 			ponteiro 	(ponteiro para a tabla auxiliar referente) == -1 se nao tem tabla referente
+ * author: Isabella e Monica
+ */
+function inserirTablaTokens(tablaTokens, idToken, lineaCodigo, colunaCodigo, ponteiro){
+	var idLinea = tablaTokens.length;
+	
+	tablaTokens[idLinea] = new Array();
+	tablaTokens[idLinea][0] = idLinea;
+	tablaTokens[idLinea][1] = idToken;
+	tablaTokens[idLinea][2] = lineaCodigo;
+	tablaTokens[idLinea][3] = colunaCodigo;
+	tablaTokens[idLinea][4] = ponteiro;
+	
+}
+
+/**
+ * Função inserirTablaLiterales: insere um novo token na tabla de literais
+ * author: Isabella e Monica
+ */
+function inserirTablaLiterales(tablaLiterales, literal, lineaCodigo, colunaCodigo) {
+	var idLinea = tablaLiterales.length;
+	
+	tablaLiterales[idLinea] = new Array();
+	tablaLiterales[idLinea][0] = idLinea;
+	tablaLiterales[idLinea][1] = literal;
+	tablaLiterales[idLinea][2] = lineaCodigo;
+	tablaLiterales[idLinea][3] = colunaCodigo;
+
+	return idLinea;
+}
+
+/**
+ * Função inserirTablaIdentificadores: insere um novo token na tabla de identificadores
+ * author: Isabella e Monica
+ */
+function inserirTablaIdentificadores(tablaIdentificadores, nomeIdentificador, lineaCodigo, colunaCodigo) {
+	var idLinea = tablaIdentificadores.length;
+	var variable = false;
+	var id;
+	
+	for (var i=0; i<idLinea; i++) {
+		if (tablaIdentificadores[i][1] == nomeIdentificador) {
+			variable = true;
+			id = tablaIdentificadores[i][0];
+		}
+	}
+	
+	if (variable == false) {
+		tablaIdentificadores[idLinea] = new Array();
+		tablaIdentificadores[idLinea][0] = idLinea;
+		tablaIdentificadores[idLinea][1] = nomeIdentificador;
+		tablaIdentificadores[idLinea][2] = lineaCodigo;
+		tablaIdentificadores[idLinea][3] = colunaCodigo;
+		//tablaIdentificadores[idLinea][2] = fkTablaConstantes;		//Valor da variável 
+		return idLinea;
+	} else {
+		return id;
+	}
+	
+}
+
+/**
+ * Função inserirTablaConstantes: insere um novo token na tabla de constantes
+ * author: Isabella e Monica
+ */
+function inserirTablaConstantes(tablaConstantes, constante, lineaCodigo, colunaCodigo) {
+	var idLinea = tablaConstantes.length;
+	tablaConstantes[idLinea] = new Array();
+	tablaConstantes[idLinea][0] = idLinea;
+	tablaConstantes[idLinea][1] = constante;
+	tablaConstantes[idLinea][2] = lineaCodigo;
+	tablaConstantes[idLinea][3] = colunaCodigo;	
+	//tablaConstantes[idLinea][2] = fkTablaIndetificadores;		//Variável referente ao valors
+	return idLinea;
+}
+
+
+/**
+ * Função inserirtablaErrores: insere o erro na tabla de erros
+ * author: Isabella e Monica
+ */
+function inserirtablaErrores(tablaErrores, descripcionDelError, lineaCodigo, colunaCodigo) {
+	var idLinea = tablaErrores.length;
+	tablaErrores[idLinea] = new Array();
+	tablaErrores[idLinea][0] = idLinea;
+	tablaErrores[idLinea][1] = descripcionDelError;
+	tablaErrores[idLinea][2] = lineaCodigo;
+	tablaErrores[idLinea][3] = colunaCodigo;
+
+	return idLinea;
+}
+
+
+/***
+ * author: Monica
+ */
+function exibirTablas(tablaTokens, tablaConstantes, tablaLiterales, tablaIdentificadores, tablaErroresLexicos){
+	var mensaje = "";
+	var clase = "";
+	var div = "";
+	
+	div = "<h5>Tabla de Constantes</h5>";
+	div = div + "<table class='table'><thead><tr><th>id</th><th>Constante</th><th>Linea</th><th>Coluna</th></tr></thead><tbody>";
+	
+	if (tablaConstantes.length > 0) {
+		for(var i = 0; i < tablaConstantes.length; i++) {
+			div = div.concat("<tr class='line-hover'>");
+			div = div.concat("<td>" + tablaConstantes[i][0] + "</td>");
+			div = div.concat("<td>" + tablaConstantes[i][1] + "</td>");
+			div = div.concat("<td>" + tablaConstantes[i][2] + "</td>");
+			div = div.concat("<td>" + tablaConstantes[i][3] + "</td>");
+			div = div.concat("</tr>");
+		}	
+		div = div + "</tbody></table>";
+		var divTablaConstantes = document.getElementById('tablaConst');
+		divTablaConstantes.innerHTML = div;
+		divTablaConstantes.classList.remove("ocultar-div");
+	}
+	
+	div = "<h5>Tabla de Literales</h5>";
+	div = div + "<table class='table'><thead><tr><th>id</th><th>Literal</th><th>Linea</th><th>Coluna</th></tr></thead><tbody>";
+	
+	if (tablaLiterales.length > 0) {
+		for(var i = 0; i < tablaLiterales.length; i++) {
+			div = div.concat("<tr class='line-hover'>");
+			div = div.concat("<td>" + tablaLiterales[i][0] + "</td>");
+			div = div.concat("<td>" + tablaLiterales[i][1] + "</td>");
+			div = div.concat("<td>" + tablaLiterales[i][2] + "</td>");
+			div = div.concat("<td>" + tablaLiterales[i][3] + "</td>");
+			div = div.concat("</tr>");
+		}	
+		div = div + "</tbody></table>";
+		var divTablaLiterales = document.getElementById('tablaLite');
+		divTablaLiterales.innerHTML = div;
+		divTablaLiterales.classList.remove("ocultar-div");
+	}
+	
+	div = "<h5>Tabla de Identificadores</h5>";
+	div = div + "<table class='table'><thead><tr><th>id</th><th>Identificador</th><th>Linea</th><th>Coluna</th></tr></thead><tbody>";
+	
+	if (tablaIdentificadores.length > 0) {
+		for(var i = 0; i < tablaIdentificadores.length; i++) {
+			div = div.concat("<tr class='line-hover'>");
+			div = div.concat("<td>" + tablaIdentificadores[i][0] + "</td>");
+			div = div.concat("<td>" + tablaIdentificadores[i][1] + "</td>");
+			div = div.concat("<td>" + tablaIdentificadores[i][2] + "</td>");
+			div = div.concat("<td>" + tablaIdentificadores[i][3] + "</td>");
+			div = div.concat("</tr>");
+		}	
+		div = div + "</tbody></table>";
+		var divTablaIdentificadores = document.getElementById('tablaVariables');
+		divTablaIdentificadores.innerHTML = div;	
+		divTablaIdentificadores.classList.remove("ocultar-div");		
+	}
+	
+	//div = "<h5>Tabla de Errores Léxicos</h5>";
+	div = "";
+	div = div + "<table class='hoverable centered responsive-table'><thead><tr><th>id</th><th>Erro</th><th>Linea</th><th>Coluna</th></tr></thead><tbody>";
+	
+	if (tablaErroresLexicos.length > 0) {
+		for(var i = 0; i < tablaErroresLexicos.length; i++) {
+			div = div.concat("<tr class='line-hover'>");
+			div = div.concat("<td>" + tablaErroresLexicos[i][0] + "</td>");
+			div = div.concat("<td>" + tablaErroresLexicos[i][1] + "</td>");
+			div = div.concat("<td>" + tablaErroresLexicos[i][2] + "</td>");
+			div = div.concat("<td>" + tablaErroresLexicos[i][3] + "</td>");
+			div = div.concat("</tr>");
+		}	
+		div = div + "</tbody></table>";
+
+		var divtablaErroresLexicos = document.getElementById('tablaErroresLexicos');
+		divtablaErroresLexicos.innerHTML = div;	
+		divtablaErroresLexicos.classList.remove("ocultar-div");	
+		//mensaje = "Ohhh! Seu código possui erros léxicos :(";	
+		clase = "flash-fail";
+	} else {
+		div = "<p>Parabéns! Seu código não possui erros léxicos :)</p>";
+		clase = "flash-success";	
+		var divtablaErroresLexicos = document.getElementById('tablaErroresLexicos');
+		divtablaErroresLexicos.innerHTML = div;	
+		divtablaErroresLexicos.classList.remove("ocultar-div");
+	}	
+
+	var spanErroresLexico = document.getElementById('lexico-erros');
+	spanErroresLexico.innerHTML = tablaErroresLexicos.length;		
+	spanErroresLexico.classList.add(clase);
+
+	var results = document.getElementById('results-div')
+	results.classList.remove("ocultar-div");				
+}
+
+function exibirErroresSintaticos(){
+	var div = "";
+	div = div + "<table class='hoverable centered responsive-table'><thead><tr><th>id</th><th>Erro</th><th>Linea</th><th>Coluna</th></tr></thead><tbody>";
+	if (tablaErroresSintaticos.length > 0){
+		
+		for(var i = 0; i < tablaErroresSintaticos.length; i++) {
+			div = div.concat("<tr class='line-hover'>");
+			div = div.concat("<td>" + tablaErroresSintaticos[i][0] + "</td>");
+			div = div.concat("<td>" + tablaErroresSintaticos[i][1] + "</td>");
+			div = div.concat("<td>" + tablaErroresSintaticos[i][2] + "</td>");
+			div = div.concat("<td>" + tablaErroresSintaticos[i][3] + "</td>");
+			div = div.concat("</tr>");
+		}	
+		div = div + "</tbody></table>";
+		var divtablaErroresSintatico = document.getElementById('tablaErroresSintatico');
+		divtablaErroresSintatico.innerHTML = div;	
+		divtablaErroresSintatico.classList.remove("ocultar-div");	
+		mensaje = "Ohhh! Seu código possui erros sintáticos :(";	
+		classe = "flash-fail";
+		
+	} else {
+		div = "<p>Parabéns! Seu código não possui erros sintáticos :)</p>";
+		classe = "flash-success";	
+		var divtablaErroresLexicos = document.getElementById('tablaErroresSintatico');
+		divtablaErroresLexicos.innerHTML = div;	
+		divtablaErroresLexicos.classList.remove("ocultar-div");
+	}		
+
+	var spanErroresSintatico = document.getElementById('sintatico-erros');
+	spanErroresSintatico.innerHTML = tablaErroresSintaticos.length;	
+	spanErroresSintatico.classList.add(classe);
+}
--- src/routes/logica.js
+++ src/routes/logica.js
@@ -18,3 +18,534 @@ var palabrasReservadas = {"funcion" : 18, "principal" : 19, "retornar" : 20, "va
 var literales = {'"' : 37, "'" : 38};
 var agrupacion = {"{" : 39, "}" : 40, "(" : 41, ")" : 42, ',' : 43, "." : 44};				
 var puntoYComa = {";" : 45};
+
+
+
+/**
+ * Crea una tabla donde se inserta el codigo que ya ha sido compilado. 
+ * @param {S} archivo 
+ */
+function crearCodigoCompilado(archivo){
+	codigoCompilado = codigoCompilado.concat("<table>");
+	
+	//Lineas de archivo
+	for(var i = 0; i < archivo.length; i++) {
+		codigoCompilado = codigoCompilado.concat("<tr id='linea-" + i + "'>");
+		
+		//Colunas do archivo
+		for (var j = 0; j < archivo[i].length; j++) {
+			codigoCompilado = codigoCompilado.concat("<td id='celula-" + i + "-" + j+ "'>" + 
+			archivo[i][j] + "</td>");
+		}
+		
+		codigoCompilado = codigoCompilado.concat("</tr>");
+	}
+	codigoCompilado = codigoCompilado.concat("</table>");
+	exhibirCodigoCompilado();
+}
+
+/**
+ * Devuelve el siguiente token de una linea
+ * @param {*} linea  
+ *      la linea actual de lectura 
+ */
+function getNextToken(linea) {
+	var token;
+	linea.value++;
+	
+	if (linea.value < tablaTokens.length)
+		token = tablaTokens[linea.value][1];
+	else token = null;
+	
+	return token;
+}
+
+/**
+ * Funcion que trata al tipo de token declaracion
+ * @param {*} token 
+ * @param {*} linea 
+ * @param {*} tieneError 
+ */
+function declaration (token , linea, tieneError) {
+	 var columna;
+	token = getNextToken(linea); 
+	 //Se é um identificador
+	 if (token == -1){
+		token = getNextToken(linea);
+		
+		if (token != 31){		
+			if (token != null) columna = tablaTokens[linea.value][3];
+			else columna = tablaTokens[linea.value-1][3];
+			
+			if (token == 36) descripcionDelError = "No se permiten múltiples declaraciones en una línea.";
+			else if (token == 2) descripcionDelError = "Las asignaciones en la línea de declaración no están permitidas.";
+			else {
+				linea.value--;
+				columna = columna + 1;
+				descripcionDelError = "La declaración debe terminar con un punto y coma.";
+			}
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], columna);
+			tieneError.value = 1;
+		}  		
+	 } else {
+		descripcionDelError = "Declaracion Incorrecta! Depois de um tipo deve ser um identificador.";
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	 }	 
+}
+
+/**
+ * Funcion que trata a una expresion de asignacion
+ * @param {*} token 
+ * @param {*} linea 
+ * @param {*} tieneError 
+ */
+function assignment_expression (token , linea, tieneError) {
+	 token = getNextToken(linea);
+	 	 
+	 //Se é um sinal de atribução =
+	 if (token == 2){
+		token = getNextToken(linea);
+		assignment_expression_prime(token , linea, tieneError);		
+		
+	 } else {
+		descripcionDelError = "Asignación incorrecta! Después de que un identificador debe contener un signo de asignación";
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	 }	 
+}
+
+/**
+ * Verifa la estructura de una operacion aritmetica
+ * @param {]} token 
+ * @param {*} linea 
+ * @param {*} tieneError 
+ */
+function operacion_aritmetica (token , linea, tieneError) {
+
+	 //Operadores aritméticos
+	 if (token >= 12 && token <= 17){
+		token = getNextToken(linea);
+		
+		//Identificador o constante
+		if (token == -1 || token == -2){
+			token = getNextToken(linea);			
+			operacion_aritmetica (token , linea, tieneError);
+			
+		} else {
+			descripcionDelError = "La operación aritmética debe tener un operador entre dos factores.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+		}
+		
+	//ERROR: Debe ser punto y coma
+	 } else if (token != 45) {
+			linea.value--;
+			descripcionDelError = "La tarea debe terminar con un punto y coma.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+	 }
+}
+
+/***
+ * Função 
+ * author:
+ */
+function assignment_expression_prime (token , linea, tieneError) {
+	
+	//Literales
+    if (token == 37 || token == 38){
+		token = getNextToken(linea);
+		
+		if (token != 31){
+			linea.value--;
+			descripcionDelError = "La tarea debe terminar con un punto y coma.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+		}
+		
+	//Identificador o constante
+	 } else if (token == -1 || token == -2) {
+		token = getNextToken(linea);		
+		operacion_aritmetica (token , linea, tieneError);
+	
+	//ERROR DE ENTRADA
+	 } else {
+			descripcionDelError = "La asignación es incorrecta!";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+	 }
+}
+
+
+/***
+ * Evalua la funcion imprimir
+ */
+function imprimir_statement (token , linea, tieneError) {
+	 token = getNextToken(linea);
+	 	 
+	 if (token == 29 || token==30 || token==-1){
+	 	token = getNextToken(linea);
+		if (token != 31){
+			linea.value--;
+			coumna = tablaTokens[linea.value][3] + 1;
+			descripcionDelError = "La declaración debe terminar con un punto y coma.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], coumna);
+			tieneError.value = 1;
+		} 		
+	} else {
+		if (token == 34) descripcionDelError = "String no permitido entre parentesis";
+		else descripcionDelError = "¡Declaración incorrecta! La declaración debe ser de tipo print literal";
+
+		if (token != null) 
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		else {
+			linea.value--
+			coumna = tablaTokens[linea.value][3] + 1;
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], coumna);
+			linea.value++
+		}
+		tieneError.value = 1;
+	 }	 
+}
+
+
+/***
+ * Função que verifica se e uma expressao de comparacao ou uma expressao logica entre um identificador e/ou uma constante
+ * author: Isabella
+ */
+function expression (token, linea, tieneError) {
+	if (token == -1 || token == -2){	// si y un identificador o una constante
+		token = getNextToken(linea);		
+		if (token >= 3 && token <= 10)	{	// si eres un operador de comparación (<, >, <=, >=, ==, !=, ||, &&)
+			token = getNextToken(linea);
+			if (token != -1 && token != -2) {	// si no es una constante o un identificador
+				descripcionDelError = "La comparación solo se puede hacer con constante y / o identificador";
+				insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+				tieneError.value = 1;
+			}
+		} else if (token == 11) {	// si es un símbolo de negación (!)
+			token = getNextToken(linea);
+			if (token != -1) {	// si no es un identificador
+				descripcionDelError = "La negación debe hacerse de la siguiente manera: !identificador.";
+				insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+				tieneError.value = 1;
+			} 
+		} else {
+			descripcionDelError = "La negación debe hacerse de la siguiente manera: !identificador.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+		}
+	} else if (token == 11) {	//si es un símbolo de negación (!)
+		token = getNextToken(linea);
+		if (token != -1) {	// si no es un identificador
+			descripcionDelError = "La negación debe hacerse de la siguiente manera: !identificador.";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+		}
+	} else {
+		descripcionDelError = "Expresion inválida.";
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	}
+}
+
+/***
+ * 
+ * 
+ */
+function code_snippet (token, linea, tieneError) {
+	var lineaAnterior = linea.value;	
+	while (token != 33 && token != null) {						// La negación debe hacerse a continuación:	
+		if (token == 20 || token == 21){						
+			token = getNextToken(linea);
+			if (token != 31) {
+				linea.value--
+				coumna = tablaTokens[linea.value][3] + 1;
+				descripcionDelError = "Deve haver um ponto e virgula depois do statement";
+				insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], coumna);	
+				linea.value++
+				tieneError.value = 1;
+			}
+		} else 	{
+			statement(token, linea);
+		}
+		token = getNextToken(linea);
+	}	
+	
+	if (token != 33){
+		linea.value = lineaAnterior-1;
+		descripcionDelError = "Debe haber una tecla de cierre después de un bloque de código..";
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);	
+		linea.value = tablaTokens.length;
+	} else {
+		linea.value = linea.value -1;
+	}
+}
+
+/***
+ * 
+ * 
+ * 
+ */
+function mientras_statement (token , linea, tieneError) {
+	 token = getNextToken(linea);
+
+	 if (token == 34) {	// Se abre parentesis
+	 	token = getNextToken(linea);
+	 	expression(token , linea, tieneError);
+	 	token = getNextToken(linea);
+	 	if (token == 35) {		// Se fecha parentesis
+	 		token = getNextToken(linea);
+	 		if (token == 32){	// Se abre llave
+	 			token = getNextToken(linea);
+	 			code_snippet(token , linea, tieneError);	// fecha llave dentro do code_snippet
+	 			token = getNextToken(linea);
+			} else {
+				descripcionDelError = " Despues de la expresion, abrir la llave";
+				insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+				tieneError.value = 1;
+	 		}
+	 	} else {
+			descripcionDelError = "Debe cerrar los paréntesis después de una expresión..";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+ 		}
+
+	 } else {
+		descripcionDelError = 'Declaracion Incorrecta! La declaración debe tener un paréntesis.';
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	}
+}
+
+/***
+ * Função que trata o if
+ * author: Isabella e Monica
+ * TODO: Nao pode vir else antes de else if
+ */
+function if_statement_linea (token , linea, tieneError) {
+	if (token == 32){	// Se abre llave
+		token = getNextToken(linea);
+		code_snippet(token , linea, tieneError);	// fecha llave dentro do code_snippet
+		token = getNextToken(linea);
+	} else {
+		descripcionDelError = " Después de la expresión, abra la llave.";
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	}
+}
+
+
+/***
+ * Função que trata o if
+ * author: Isabella e Monica
+ * TODO: Nao pode vir else antes de else if
+ */
+function if_statement (token , linea, tieneError) {
+	token = getNextToken(linea);
+
+	if (token == 34) {	// Se abre parentesis
+	 	token = getNextToken(linea);
+	 	expression(token , linea, tieneError);
+	 	token = getNextToken(linea);
+	 	if (token == 35) {		// Se fecha parentesis
+	 		token = getNextToken(linea);
+	 		if (token == 32){	// Se abre llave
+	 			token = getNextToken(linea);
+	 			code_snippet(token , linea, tieneError);	// fecha llave dentro do code_snippet
+	 			token = getNextToken(linea);
+			 	if (token == 19) {	// Se e else
+					token = getNextToken(linea);
+					if (token == 18){		//se for if
+						if_statement (token , linea, tieneError);
+					} else {
+						 if_statement_linea (token , linea, tieneError) 
+					}
+	 			} 
+	 		} else {
+				descripcionDelError = " Después de la expresión, abra la llave.";
+				insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+				tieneError.value = 1;
+	 		}
+	 	} else {
+			descripcionDelError = "Debe cerrar los paréntesis después de una expresión..";
+			insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+			tieneError.value = 1;
+ 		}
+	 } else {
+		descripcionDelError = 'Declaracion Incorrecta! La declaración debe tener un paréntesis.';
+		insertarEnTablaDeErrores(tablaErroresSintaticos, descripcionDelError, tablaTokens[linea.value][2], tablaTokens[linea.value][3]);
+		tieneError.value = 1;
+	}
+}
+
+/***
+ * 
+ */
+function exhibirCodigoCompilado() {
+	codigoCompilado = codigoCompilado.concat("</tr></table>");
+	var divCodigoCompilado = document.getElementById('codigoCompilado');
+	divCodigoCompilado.innerHTML = codigoCompilado;
+	divCodigoCompilado.classList.remove("ocultar-div");  //le quitamos la clase que la
+}
+
+/***
+ * Função 
+ * author:
+ */
+
+function statement (token, linea) {
+	 var tieneError = {value: 0}; //mapa de errores
+
+     //Si el token es una declaracion
+	 if (token >= 22 && token <= 27){
+		declaration(token , linea, tieneError);
+		 
+	// Si es un identificador - Asignación
+	 } else if (token == -1) {
+		assignment_expression(token , linea, tieneError);
+	 // mientras
+	 } else if (token == 30) {
+		 mientras_statement(token , linea, tieneError);
+		 
+	 // imprimir 33
+	 } else if (token == 23) {
+		 imprimir_statement(token , linea, tieneError);
+		 
+	 // if - condicional
+	 } else if (token == 18) {
+		if_statement(token , linea, tieneError);
+		 
+	 }
+	 
+	 //Restaurar estado sin error, es decir, desplazarse hasta el final de la línea actual
+	 if (tieneError.value == 1){
+		 while ((linea.value < tablaTokens.length) && (tablaTokens[linea.value][3] != 0)) {
+				linea.value++;
+				// se detendrá en la siguiente línea correcta
+		 }
+		 linea.value--;
+	 }
+}
+
+/***
+ * Função 
+ * author:
+ */
+
+function sintatico () {
+	var linea = {value: 0};	
+	var token;
+
+	while (linea.value < tablaTokens.length) {
+		token = tablaTokens[linea.value][1];
+		statement(token, linea);		
+		linea.value++;			//Saltar Linea
+	}
+	 exibirErroresSintaticos();
+}
+
+
+
+
+/***
+ * Função main: função principal tratando cada caso de token
+ * author: Isabella e Monica
+ */
+function main (archivo) {
+	
+	var lineasDelArchivo = archivo.length;							// Numero de lineas do archivo de entrada
+	var vigilante = {value: 0};										// Primer Caracter leido
+	var visorAFuturo = {value: 1};									// Puntero para leer cada token
+	var flag; 														// Variable para retorno de error
+	var retorno;													// Array de funciones de retorno
+	
+	//Creación de tabla
+	retorno = crearTablas(tablaIdentificadores, tablaTokens, tablaLiterales, tablaConstantes, 
+	tablaErroresLexicos);
+
+	tablaIdentificadores = retorno[0];
+    tablaTokens = retorno[1];
+    tablaLiterales = retorno[2];
+    tablaConstantes = retorno[3];
+    tablaErroresLexicos = retorno[4];
+    tablaErroresSintaticos = retorno[5];
+    
+
+	// Analizando cada línea del archivo de entrada
+	for (var lineaActual = 0; lineaActual < lineasDelArchivo; lineaActual++) {
+
+		vigilante.value = 0;
+		visorAFuturo.value = 1;
+	
+		//Desplazarse por los caracteres de línea
+		while (vigilante.value < archivo[lineaActual].length) {
+										
+				if (archivo[lineaActual][vigilante.value] == " " || 
+					archivo[lineaActual][vigilante.value] == '\t'|| 
+					archivo[lineaActual][vigilante.value] == '\n') {
+						
+					consumirEspacos(archivo[lineaActual], vigilante, visorAFuturo);
+				}
+				
+				if (archivo[lineaActual][vigilante.value] != undefined) { 
+					
+					if (delimitadores[archivo[lineaActual][vigilante.value]] != undefined) {
+						
+						tratarDelimitadores(tablaTokens, archivo[lineaActual], lineaActual, vigilante, visorAFuturo);
+						
+					} else if (((operadores[archivo[lineaActual][vigilante.value]] != undefined) || 
+								(operadores[archivo[lineaActual][vigilante.value]+archivo[lineaActual][visorAFuturo.value]] != undefined)) && !(archivo[lineaActual][vigilante.value] == "/" &&  archivo[lineaActual][visorAFuturo.value] == "/")) {
+						
+						tratarOperadores (tablaTokens, archivo[lineaActual], vigilante, visorAFuturo, lineaActual);
+						
+					} else if (atribuicao[archivo[lineaActual][vigilante.value]] != undefined) {
+						
+						tratarAtribuicao (tablaTokens, archivo[lineaActual], lineaActual, vigilante, visorAFuturo);
+						
+					} else 	if (semicolon[archivo[lineaActual][vigilante.value]] != undefined) {
+						
+						tratarSemicolon (tablaTokens, archivo[lineaActual][vigilante.value], lineaActual, vigilante, visorAFuturo);	
+						
+					} else if (literais[archivo[lineaActual][vigilante.value]] != undefined) {
+						
+						tratarLiteral (tablaTokens, tablaLiterales, tablaErroresLexicos, archivo[lineaActual], vigilante, visorAFuturo, lineaActual);
+						
+					} else if (archivo[lineaActual][vigilante.value] == "/" && 
+							   archivo[lineaActual][visorAFuturo.value] == "/" ) {
+								   
+						tratarComentario(tablaTokens, archivo[lineaActual], vigilante, visorAFuturo, lineaActual);
+						
+					} else if (isNumeric(archivo[lineaActual][vigilante.value]))  {
+						
+						tratarNumero (tablaTokens, tablaConstantes, tablaErroresLexicos, archivo[lineaActual], vigilante, visorAFuturo, lineaActual);
+						
+					} else if (isLetter(archivo[lineaActual][vigilante.value])){
+						
+						verificarToken(tablaTokens, tablaIdentificadores, archivo[lineaActual], vigilante, visorAFuturo, 
+						lineaActual);		
+
+					} else {			
+						var descripcionDelError = "Ooops! Isso eu não conheço! Você pode verificar o erro pra mim!?";
+						insertarEnTablaDeErrores (tablaErroresLexicos, descripcionDelError, lineaActual, vigilante.value);
+						vigilante.value = archivo[lineaActual].length + 1;
+					}
+				
+			} else {
+				vigilante.value = archivo[lineaActual].length + 1;
+			}
+		}//end While
+	}//end for
+
+	var divDownload = document.getElementById('botao-download-div');
+	divDownload.classList.remove("ocultar-div");	
+
+	sintatico ();
+	
+	exibirTablas(tablaTokens, tablaConstantes, tablaLiterales, tablaIdentificadores,tablaErroresLexicos);
+	gerarArquivoTexto(tablaTokens, tablaConstantes, tablaLiterales, tablaErroresLexicos, tablaIdentificadores, tablaErroresSintaticos);
+
+}//End main
+
+
